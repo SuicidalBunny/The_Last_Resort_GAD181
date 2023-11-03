e3ade83d1f66181bdd6eb49c2f5afaae1875b474
JAMAS124 <jamespatch14@icloud.com>
2023-11-04 10:53:29 +1100

    Reimported second branch & started work on LED controller
    Brought back in the scripts from my second branch manually as it did not branch back in properly yesterday.
    
    Commenced work on getting the LED's working to show whether the player has passed or failed their task.

---
+++ The_Last_Resort_V1/Assets/Scripts/PowerShutdown/CardControls.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CardControls : MonoBehaviour, IDragHandler
+{
+    //Checks to see if the player has clicked and is dragging an item
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Tells the program to move the object wherever the mouse is dragging it
+        transform.position = eventData.position;
+    }
+}
---
+++ The_Last_Resort_V1/Assets/Scripts/PowerShutdown/KeycardReaderScript.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeycardReaderScript : MonoBehaviour
+{
+    //Timer that will be used to check how long the card has been in the swiper for
+    private float swipeTimer = 0.0f;
+    private float timer = 0.0f;
+
+    private int LEDType = 0;
+
+    public GameObject LEDOff;
+    public GameObject LEDOn;
+    public GameObject LEDFail;
+
+    //Checks if a collider is within the trigger zone
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        SwipeEnabler();
+    }
+
+    //Checks if an collider has exited the trigger
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        SwipeTimeChecker();
+    }
+
+    //Checks to start the timer for the swipe when called
+    private void SwipeEnabler()
+    {
+        //Tells the console that a trigger has been detectes
+        Debug.Log("Card Trigger Detected");
+
+        //Reads how many seconds has passed whilst in the trigger
+        swipeTimer += Time.deltaTime;
+    }
+
+    //Checks whether the player has passed or failed depending on how long it takes for the swipe
+    private void SwipeTimeChecker()
+    {
+        //Tells the console how long the card was in the trigger zone for
+        Debug.Log($"Timer = " + swipeTimer);
+
+        //Checks if the swipe timer is inbetween 0.3 and 0.4 seconds
+        if (swipeTimer > 0.3f && swipeTimer < 0.4f)
+        {
+            //Tells the console the system is meant to pass
+            Debug.Log("Pass");
+        }
+        else
+        {
+            //Tells the console the system is meant to fail
+            Debug.Log("Fail");
+        }
+
+        //Resets the timer for the next attempt
+        swipeTimer = 0.0f;
+    }
+
+    private void LEDController()
+    {
+        switch(LEDType)
+        {
+            case 0:
+                LEDOff.SetActive(true);
+                break;
+            case 1:
+                LEDOn.SetActive(true);
+                break;
+            case 2:
+                LEDFail.SetActive(true);
+                break;
+        }
+    }
+
+}

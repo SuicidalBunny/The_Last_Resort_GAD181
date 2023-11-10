5768fb4109d1cf7b7adbe0bda6850ac9032ff547
JAMAS124 <jamespatch14@icloud.com>
2023-11-10 12:27:55 +1100

    Adding some of the comments into some of the scripts
    Started adding comments into the quick scripts that were made yesterday for the money toss minigame, in order for easier understanding of the code.

--- The_Last_Resort_V1/Assets/Scripts/MoneyToss/MoneyGrabber.cs
+++ The_Last_Resort_V1/Assets/Scripts/MoneyToss/MoneyGrabber.cs
@@ -7,15 +7,19 @@ using UnityEngine.EventSystems;
 
 public class MoneyGrabber : MonoBehaviour
 {
+    //Links the script to the game manager
     public GameObject gameLink;
-
+    //Used for the mouse offset
     private Vector3 mOffset;
-
+    //Used to obtain the Z coordinates of the mouse
     private float mZCoord;
 
+    //When mouse 1 is held down it checks the positioning of the mouse and moves the object in relation to it
     private void OnMouseDown()
     {
+        //Sets the offset of the z coordinates in relation to the camera's position
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        //Moves the gameObject wherever the mouse moves to
         mOffset = gameObject.transform.position - GetMouseWorldPos();
     }
 
--- The_Last_Resort_V1/Assets/Scripts/MoneyToss/UIManager.cs
+++ The_Last_Resort_V1/Assets/Scripts/MoneyToss/UIManager.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-
+    //Links the script to the game manager
     public GameObject gameLink;
-
+    //Is used to display how much money will be displayed on the screen
     public TextMeshProUGUI moneyCounter;
-
+    //Integer that holds the amount of cash the player will have
     private int cash;
 
     // Update is called once per frame
     void Update()
     {
+        //Calls for the game to find out how much money the player has
         GetMoney();
-
+        //Converts the amount of money the player has to readable text on the UI
         moneyCounter.text = $"Money: {cash}";
     }
-
+    //When called, tells the program to find out how much money the player has
     void GetMoney()
     {
+        //Communicates to the game manager to check how much money the player has
         cash = gameLink.GetComponent<gameManager>().money;
     }
 }

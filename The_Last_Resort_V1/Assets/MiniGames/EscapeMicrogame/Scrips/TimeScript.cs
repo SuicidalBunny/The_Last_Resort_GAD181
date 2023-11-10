f0860930c070d0f28f82ea44fcde6b54a6a55f41
JAMAS124 <jamespatch14@icloud.com>
2023-11-10 16:50:33 +1100

    Added a UI with a timer & rectified hitboxes
    Added a UI that features a timer that counts down from 30 & logs that times up (Credit to Arda for making the script)
    
    Rectified the traffic hitboxes to make them a little smaller so that lightly grazing them feels less cheap.

---
+++ The_Last_Resort_V1/Assets/MiniGames/EscapeMicrogame/Scrips/TimeScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TimeScript : MonoBehaviour
+{
+    
+    public float timeLeft;
+    public bool Timeron = false;
+    public TextMeshProUGUI TimerTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timeron = true;
+        timeLeft = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Timeron)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer(timeLeft);
+            }
+            else
+            {
+                Debug.Log("Time Over");
+                timeLeft = 0;
+                Timeron = false;
+            }
+        }
+    }   
+
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerTxt.text = string.Format("Time Left: " + "{0:00}:{1:00}", minutes, seconds);
+    }
+}

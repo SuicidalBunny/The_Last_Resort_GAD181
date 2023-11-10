2305dcb447cf4ab3c9a0cb4a4e16721efb3a4cc2
JAMAS124 <jamespatch14@icloud.com>
2023-11-10 12:35:53 +1100

    Finished commenting the money toss scripts
    Added the final comments that needed to be added from yesterdays scripts that were made.

--- The_Last_Resort_V1/Assets/Scripts/MoneyToss/MoneyGrabber.cs
+++ The_Last_Resort_V1/Assets/Scripts/MoneyToss/MoneyGrabber.cs
@@ -14,7 +14,7 @@ public class MoneyGrabber : MonoBehaviour
     //Used to obtain the Z coordinates of the mouse
     private float mZCoord;
 
-    //When mouse 1 is held down it checks the positioning of the mouse and moves the object in relation to it
+    //When mouse 1 is held down it checks the positioning of the mouse and grabs the gameObject it is hovering
     private void OnMouseDown()
     {
         //Sets the offset of the z coordinates in relation to the camera's position
@@ -23,28 +23,35 @@ public class MoneyGrabber : MonoBehaviour
         mOffset = gameObject.transform.position - GetMouseWorldPos();
     }
 
+    //Grabs the mouse points (X and Y), followed by the Z axis and then returns the value 
     private Vector3 GetMouseWorldPos()
     {
+        //Creates a vector 3 to obtain the X and Y position of the mouse
         Vector3 mousePoint = Input.mousePosition;
-
+        //Grabs the z coordinates based off the already obtain Z coordinates
         mousePoint.z = mZCoord;
-
+        //Returnes the position of the mouse
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
+    //When the mouse is moved it will move the object
     private void OnMouseDrag()
     {
+        //Moves the object based off where the mouse has moved
         transform.position = GetMouseWorldPos() + mOffset;
     }
 
+    //When the money hits this trigger, it will remove the gameObject, as well as add the money to the counter
     private void OnTriggerEnter(Collider other)
     {
+        //Checks for if the gameObject is considered a bag
         if (other.gameObject.tag == "Bag")
         {
+            //Tells the console that the cash has been deposited
             Debug.Log("cash deposited");
-
+            //Tells the game manager to add $500 to the total cash amount
             gameLink.GetComponent<gameManager>().AddCash(500);
-
+            //Deletes the cash brick that has been deposited
             this.gameObject.SetActive(false);
         }
     }

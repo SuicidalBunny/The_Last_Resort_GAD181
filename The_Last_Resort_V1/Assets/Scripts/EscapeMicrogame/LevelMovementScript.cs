22da485a41340c32c7741c44bda7a465797c0fe6
JAMAS124 <jamespatch14@icloud.com>
2023-11-10 14:02:36 +1100

    Commenced adding proper assets into the Escape minigame
    Started actually placing assets and textures in the escape minigame so that it looks proper.
    Also figured out how long the level actually needs to be so that we do not run out of road.

--- The_Last_Resort_V1/Assets/Scripts/EscapeMicrogame/LevelMovementScript.cs
+++ The_Last_Resort_V1/Assets/Scripts/EscapeMicrogame/LevelMovementScript.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 public class LevelMovementScript : MonoBehaviour
 {
     private float time = 0.0f;
-    private float interpolationPeriod = 0.1f;
+    private float interpolationPeriod = 0.05f;
+    private float timer = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         //Ties the time float to deltaTime
         time += Time.deltaTime;
+        timer += Time.deltaTime;
+
+        Debug.Log(timer);
 
         //Checks to see if the timer has hit the interpolationPeriod
         if(time >= interpolationPeriod) {
             //Resets the timer to 0
             time = 0.0f;
             //Moves the object that the script is attatched to by 1 unit on the Z axis
-            transform.position += new Vector3(0, 0, 1);
+            transform.position += new Vector3(0, 0, -1);
         }
     }
 }
